cf448bdd33a0b6442a798afe0dcac814afd6c30f
Tessa Power <tessa@tessapower.co>
2021-05-05 15:49:29 +1200

    Add basic layout of webpage

---
+++ movieList/src/containers/List.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Card from '../components/Card/Card';
+
+class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const movies = await fetch('src/assets/data.json');
+    const moviesJSON = await movies.json();
+
+    if (moviesJSON) {
+      this.setState({
+        data: moviesJSON,
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { data, loading } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return data.map(movie => <Card key={ movie.id } movie={ movie } />);
+  }
+};
+
+export default List;
+
---
+++ movieList/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return <List />; 
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
