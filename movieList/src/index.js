313267b25c5cb66d31ba0dacc01b912b10d5d685
Tessa Power <tessa@tessapower.co>
2021-05-05 16:15:57 +1200

    Add titlebar to webpage

--- movieList/src/index.js
+++ movieList/src/index.js
@@ -6,6 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   return (
     <div className ='container-fluid'>
+      <nav className='navbar sticky-top navbar-light bg-dark'>
+        <h1 className='navbar-bran text-light'>movieList</h1>
+      </nav>
       <List />;
     </div>
   );
