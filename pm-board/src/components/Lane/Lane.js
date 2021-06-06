657cb0f7430c0f5d2da277a841f6ce127a77957a
Tessa Power <tessa@tessapower.co>
2021-06-07 10:49:14 +1200

    Add lane and ticket components

--- pm-board/src/components/Lane/Lane.js
+++ pm-board/src/components/Lane/Lane.js
@@ -31,12 +31,15 @@ const Alert = styled.div`
   text-align: center;
 `;
 
-const Lane = ({ tickets, loading, error, title }) => (
-  <LaneWrapper>
+const Lane = ({ laneId, tickets, loading, error, onDragStart, onDragOver, onDrop, title }) => (
+  <LaneWrapper
+    onDragOver={onDragOver}
+    onDrop={e => onDrop(e, laneId)}
+  >
     <Title>{title}</Title>
     {(loading || error) && <Alert>{loading ? 'Loading...' : error}</Alert>}
     <TicketsWrapper>
-      {tickets.map(ticket => <Ticket key={ticket.id} ticket={ticket} />)}
+      {tickets.map(ticket => <Ticket key={ticket.id} onDragStart={onDragStart} ticket={ticket} />)}
     </TicketsWrapper>
   </LaneWrapper>
 );
